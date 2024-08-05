d8ecb40be3916d33e05586bba809e6be9f4484e6
vikastunge <vilastunge@gmail.com>
2024-08-05 09:08:08 +0530

    Edited Code

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# TA2-independence2024
-Official repository of the TA-2 course in the independence 2024 term.
+# TA-2-independence24
+ official work of TA2 from independence24
---
+++ main.py
@@ -0,0 +1,33 @@
+from data_loader import DataLoader
+from file_reader import FileReader
+from data_merger import DataMerger
+from user_input_handler import UserInputHandler
+import pandas as pd
+
+def main():
+    file_paths = UserInputHandler.get_file_paths_from_input()
+    print(type(file_paths))
+
+    if len(file_paths) > 1:
+        if_yes = input("Do you want to merge the files side-by-side? (yes/no): ")
+        if if_yes.lower() == "yes":
+            merged_df = DataMerger.merge_files_side_by_side(file_paths)
+            if merged_df is not None:
+                print(merged_df.head())
+            else:
+                print("No DataFrames to merge.")
+        elif if_yes.lower() == "no":
+            loaded_files = FileReader.read_multiple_files(file_paths)
+            for idx, file_data in enumerate(loaded_files):
+                variable_name = f"file_{idx + 1}"
+                globals()[variable_name] = file_data
+                print(f"{variable_name}:\n{file_data}\n")
+    else:
+        single_file = DataLoader.load_data(file_paths[0])
+        print(single_file)
+        if isinstance(single_file, pd.DataFrame):
+            print(single_file.head())
+
+if __name__ == "__main__":
+    main()
+
