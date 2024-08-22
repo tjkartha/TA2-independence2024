cfcdf299c20b49c0032a1afa1f18c6efb83bffae
Thejus R. Kartha <33420541+tjkartha@users.noreply.github.com>
2024-08-22 10:18:03 +0530

    Model-eval branch integration
    `main.py`:
    - `print` statements to show the flow of control of the program
    - Detected an issue with categorical encoding, added a few print statements to check for null values
    - The `model_select` function from the library `model_selection` has been incorporated
    - Issues with categorical encoding flagged, under review now

--- main.py
+++ main.py
@@ -5,36 +5,48 @@ warnings.filterwarnings("ignore")
 from data_loader import DataHandler
 from csv_analysis import CSVAnalysis
 from Data_pre_processing_module_v2 import *
-from test import *
+from model_selection import *
 
 def main():
     
     # input----
+    print("\n--------------- input --------------------------")
     handler = DataHandler()
     df = handler.handle_files()
 
     # eda-viz----
+    print("\n--------------- eda-viz ------------------------")
     csv = CSVAnalysis()
     csv.perform_eda(df)
 
     # preproc----
+    print("\n--------------- preproc------------------------")
     preprocessor = DataPreProcessor(df)
     preprocessed_ = preprocessor.data_pre_processed()
     df = preprocessed_[0]
 
     # eda-viz----
+    print("\n--------------- eda-viz ------------------------")
     plots_storage = "./viz/"
     # if(os.path.isdir('new_folder')==False)
     # os.mkdir(plots_storage)
     # csv.interactive_plot_selection(df, plots_storage)
 
     # preproc----
+    print("\n--------------- preproc ------------------------")
+    print("**********************")
+    print("NaN values: \n", df.isnull().sum())
+    print("**********************\n")
     transform_ = DataTransformation(df)
     df = transform_.encoding_cat(cat_cols=preprocessed_[1])
-    # print(df)
+    print("\n Printing the columns:")
+    print(df.info())
+    print("\n------------\n")
+    print("NaN values: \n", df.isnull().sum())
 
     # model-eval----
-    model_eval(df)
+    print("\n--------------- model-eval ---------------------")
+    model_select(df)
 
 if __name__ == "__main__":
     main()
